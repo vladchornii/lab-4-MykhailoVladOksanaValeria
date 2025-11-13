96b46963e1f45f78b015f7501a92f1ee3ad73ec0
vladchornii <vladchorniy1@gmail.com>
2025-11-14 01:02:11 +0200

    Fixes CSS:DOM, Dockerfile Misconfiguration: Dependency Confusion, Key Management: Hardcoded Encryption Key

--- src/main/resources/lessons/challenges/js/challenge8.js
+++ src/main/resources/lessons/challenges/js/challenge8.js
@@ -1,57 +1,71 @@
 $(document).ready(function () {
     loadVotes();
     average();
-})
+});
 
 function loadVotes() {
     $.get("challenge/8/votes/", function (votes) {
-            var totalVotes = 0;
-            for (var i = 1; i <= 5; i++) {
-                totalVotes = totalVotes + votes[i];
-            }
-            console.log(totalVotes);
-            for (var i = 1; i <= 5; i++) {
-                var percent = votes[i] * 100 / totalVotes;
-                console.log(percent);
-                var progressBar = $('#progressBar' + i);
-                progressBar.width(Math.round(percent) * 2 + '%');
-                $("#nrOfVotes" + i).html(votes[i]);
+        var totalVotes = 0;
 
-            }
+        for (var i = 1; i <= 5; i++) {
+            totalVotes = totalVotes + votes[i];
         }
-    );
+
+        // Захист від ділення на нуль
+        if (totalVotes === 0) {
+            for (var j = 1; j <= 5; j++) {
+                $('#progressBar' + j).width('0%');
+                $("#nrOfVotes" + j).text(0);
+            }
+            return;
+        }
+
+        for (var i = 1; i <= 5; i++) {
+            var percent = votes[i] * 100 / totalVotes;
+            var progressBar = $('#progressBar' + i);
+            progressBar.width(Math.round(percent) * 2 + '%');
+            $("#nrOfVotes" + i).text(votes[i]);
+        }
+    });
 }
 
 function average() {
     $.get("challenge/8/votes/average", function (average) {
-            for (var i = 1; i <= 5; i++) {
-                var number = average["average"];
-                $("#star" + i).removeClass('btn-warning');
-                $("#star" + i).removeClass('btn-default');
-                $("#star" + i).removeClass('btn-grey');
+        var number = average["average"];
 
-                if (i <= number) {
-                    $("#star" + i).addClass('btn-warning');
-                } else {
-                    $("#star" + i).addClass('btn-grey');
-                }
+        for (var i = 1; i <= 5; i++) {
+            var star = $("#star" + i);
+            star.removeClass('btn-warning btn-default btn-grey');
+
+            if (i <= number) {
+                star.addClass('btn-warning');
+            } else {
+                star.addClass('btn-grey');
             }
         }
-    );
+    });
 }
 
-
 function doVote(stars) {
-    $("#voteResultMsg").hide();
-    $.get("challenge/8/vote/" + stars, function (result) {
+    var $msg = $("#voteResultMsg");
+
+    $msg.hide()
+            .removeClass('alert-danger alert-success alert-dismissable');
+
+    $.get("challenge/8/vote/" + encodeURIComponent(stars), function (result) {
+
         if (result["error"]) {
-            $("#voteResultMsg").addClass('alert-danger alert-dismissable');
+            $msg.addClass('alert-danger alert-dismissable');
         } else {
-            $("#voteResultMsg").addClass('alert-success alert-dismissable');
+            $msg.addClass('alert-success alert-dismissable');
         }
-        $("#voteResultMsg").html(result["message"]);
-        $("#voteResultMsg").show();
-    })
-    loadVotes();
-    average();
+
+        // 🔐 ФІКС XSS: використовуємо text() замість html()
+        $msg.text(result["message"]);
+        $msg.show();
+
+        // Оновлюємо лише після успішної відповіді сервера
+        loadVotes();
+        average();
+    });
 }
